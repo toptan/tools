dd07f14638dacf68acd80775d1709363d3e8f827
Toplica Tanasković <toplicius@gmail.com>
2015-02-17 10:56:31 +0100

    Fixed vim setup

--- setup-vim.sh
+++ setup-vim.sh
@@ -5,4 +5,6 @@ rm -rf ~/.vim
 echo "Setting new vim configuration"
 ln -s `pwd`/vim/.vimrc ~/.vimrc
 ln -s `pwd`/vim/.vim ~/.vim
+echo "Adding NeoBundle..."
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 echo "Done."
